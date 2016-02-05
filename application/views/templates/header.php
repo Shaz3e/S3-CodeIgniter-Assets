e6a06e771fd37e10165b0dd90e03ed639ab1ba6f
Shahrukh A. Khan <iam@shaz3e.com>
2016-02-05 20:20:55 +0500

    New Version Updates

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -38,17 +38,17 @@
 <?php
 /**
  * Less Styles 
- * comment below lines on production
+ * comment below lines on production machine
  */
 ?>
-<link href="<?php echo asset_url('themes/style1/style.less'); ?>" rel="stylesheet/less" type="text/css">
+<link href="<?php echo asset_url('themes/styles/theme1.less'); ?>" rel="stylesheet/less" type="text/css">
 <script type="text/javascript" src="<?php echo asset_url('js/less.js'); ?>"></script>
 
 <?php
 	require('assets/lessc.inc.php');
 
-	$inputFile = "assets/themes/style1/style.less";
-	$outputFile = "assets/themes/style1/style.css";
+	$inputFile = "assets/themes/styles/theme1.less";
+	$outputFile = "assets/themes/style.css";
 	
 	$less = new lessc;
 	$less->setFormatter("compressed");
@@ -63,27 +63,31 @@
 		file_put_contents($outputFile, $cache["compiled"]);
 	}
 	
-	echo '<link href="' . asset_url('themes/style1/style.css') . '" rel="stylesheet" type="text/css">';
+	echo '<link href="' . asset_url('themes/style.css') . '" rel="stylesheet" type="text/css">';
 ?>
 
 <?php // Favicons ?>
 <link rel="shortcut icon" href="<?php echo asset_url('images/favicon/favicon.ico'); ?>">
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo asset_url('images/favicon/apple-touch-icon-57x57.png'); ?>">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo asset_url('images/favicon/apple-touch-icon-114x114.png'); ?>">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo asset_url('images/favicon/apple-touch-icon-72x72.png'); ?>">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo asset_url('images/favicon/apple-touch-icon-144x144.png'); ?>">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo asset_url('images/favicon/apple-touch-icon-60x60.png'); ?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo asset_url('images/favicon/apple-touch-icon-120x120.png'); ?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo asset_url('images/favicon/apple-touch-icon-76x76.png'); ?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo asset_url('images/favicon/apple-touch-icon-152x152.png'); ?>">
-<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-196x196.png" sizes="196x196'); ?>">
-<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-160x160.png" sizes="160x160'); ?>">
-<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-96x96.png" sizes="96x96'); ?>">
-<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-16x16.png" sizes="16x16'); ?>">
-<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-32x32.png" sizes="32x32'); ?>">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?php echo asset_url('images/favicon/mstile-144x144.png'); ?>">
-<meta name="msapplication-config" content="<?php echo asset_url('images/favicon/browserconfig.xml'); ?>">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo asset_url('images/favicon/apple-touch-icon-57x57.png');?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo asset_url('images/favicon/apple-touch-icon-60x60.png');?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo asset_url('images/favicon/apple-touch-icon-72x72.png');?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo asset_url('images/favicon/apple-touch-icon-76x76.png');?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo asset_url('images/favicon/apple-touch-icon-114x114.png');?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo asset_url('images/favicon/apple-touch-icon-120x120.png');?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo asset_url('images/favicon/apple-touch-icon-144x144.png');?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo asset_url('images/favicon/apple-touch-icon-152x152.png');?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo asset_url('images/favicon/apple-touch-icon-180x180.png');?>">
+<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-32x32.png" sizes="32x32');?>">
+<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/android-chrome-192x192.png" sizes="192x192');?>">
+<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-96x96.png" sizes="96x96');?>">
+<link rel="icon" type="image/png" href="<?php echo asset_url('images/favicon/favicon-16x16.png" sizes="16x16');?>">
+<link rel="manifest" href="<?php echo asset_url('images/favicon/manifest.json');?>">
+<link rel="mask-icon" href="<?php echo asset_url('images/favicon/safari-pinned-tab.svg');?>" color="#5bbad5">
+<link rel="shortcut icon" href="<?php echo asset_url('images/favicon/favicon.ico');?>">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="<?php echo asset_url('images/favicon/mstile-144x144.png');?>">
+<meta name="msapplication-config" content="<?php echo asset_url('images/favicon/browserconfig.xml');?>">
+<meta name="theme-color" content="#ffffff">
 
 <!--[if lt IE 9]>
     <script type="text/javascript" src="<?php echo asset_url('js/html5.js'); ?>"></script>
@@ -118,7 +122,7 @@
     <section class="dc-header dc-clear" id="dc-header">
         <div class="row">
         	
-            <div class="dc-logo"><a href="<?php echo base_url(); ?>"><img src="<?php echo asset_url('themes/style1/images/logo.png'); ?>"></a></div>
+            <div class="dc-logo"><a href="<?php echo base_url(); ?>"><img src="<?php echo asset_url('themes/images/logo.png'); ?>"></a></div>
 
         </div>
     </section>
